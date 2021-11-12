4097e1d63f5b15162e964a4543c87967492dd523
Nabuuso Lizzie <betty.nabuuso@student.moringaschool.com>
2021-11-12 14:39:08 +0300

    Hashed password

--- User-Pitches/app.py
+++ User-Pitches/app.py
@@ -2,6 +2,7 @@ from flask import Flask,render_template,flash,request,jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from datetime import datetime
+from werkzeug.security import generate_password_hash,check_password_hash
 
 app = Flask(__name__)
 app.config['SECRET_KEY']="Abcd1234!@#$%^&*()EFGH"
@@ -13,15 +14,34 @@ class User(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     full_name = db.Column(db.String(200),nullable=False)
     email = db.Column(db.String(200),nullable=False,unique=True)
-    password = db.Column(db.String(200),nullable=False)
+    password_hash = db.Column(db.String(200))
     created_date = db.Column(db.DateTime,default=datetime.utcnow)
-    def __init__(self,full_name,email,password):
-        self.full_name = full_name
-        self.email = email
-        self.password = password
+    # def __init__(self,full_name,email,password):
+    #     self.full_name = full_name
+    #     self.email = email
+    #     self.password = password
+    
+    @property
+    def password(self):
+        raise AttributeError('Password is not a readable attribute')
+    @password.setter
+    def password(self,password):
+        self.password_hash = generate_password_hash(password)
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
     def __repr__(self):
         return '<Name %r>' % self.full_name
-
+class PitchCategory(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    category_name = db.Column(db.String(200),nullable=False)
+    created_date = db.Column(db.DateTime,default=datetime.utcnow)
+class Pitch(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(200),nullable=False)
+    description = db.Column(db.Text)
+    created_date = db.Column(db.DateTime,default=datetime.utcnow)
+    upvote = db.Column(db.Integer)
+    downvote = db.Column(db.Integer)
 @app.route('/index')
 @app.route('/')
 def index():
@@ -41,11 +61,11 @@ def register():
 def users():
     full_name = request.form['full_name']
     email = request.form['email']
-    password = request.form['password']
-    user = User(full_name=full_name,email=email,password=password)
+    hashed_password = generate_password_hash(request.form['password'],"sha256")
+    user = User(full_name=full_name,email=email,password_hash=hashed_password)
     db.session.add(user)
     db.session.commit()
-    return jsonify({'name':full_name,'email':email,password:'password'})
+    return jsonify({'name':full_name,'email':email})
 
 if __name__ == '__main__':
     db.create_all()
